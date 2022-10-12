a77661386ed2aee18a977b23d890c26db819a7d5
Chungryeol Lee <chulee@c10r8s5.42seoul.kr>
2022-10-12 20:07:40 +0900

    cub3d

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: chulee <chulee@student.42seoul.kr>         +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/26 19:41:21 by chulee            #+#    #+#              #
-#    Updated: 2022/10/12 19:40:56 by chulee           ###   ########.fr        #
+#    Updated: 2022/10/12 20:05:53 by chulee           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,7 +17,7 @@ OBJS = $(addprefix objs/, $(notdir $(SRCS:.c=.o)))
 SRCS_B=bonus/main_bonus.c
 OBJS_B = $(addprefix objs/, $(notdir $(SRCS_B:.c=.o)))
 CC = cc
-CFLAGS = -Wall -Wextra -Werror
+CFLAGS = -fsanitize=address -g
 MLX_LIB = ./minilibx_opengl_20191021
 MLX_FLAGS = -lm -L$(MLX_LIB) -lmlx -framework OpenGL -framework AppKit
 
--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,87 +6,101 @@
 /*   By: chulee <chulee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 17:44:58 by chulee            #+#    #+#             */
-/*   Updated: 2022/10/07 19:33:07 by chulee           ###   ########.fr       */
+/*   Updated: 2022/10/12 20:06:28 by chulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB_3D_H
 # define CUB_3D_H
 
-# include "mlx.h"
-# include "get_next_line.h"
+# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <fcntl.h>
+# include <float.h>
+# include <math.h>
+# include <string.h>
+# include <stdbool.h>
+# include "get_next_line.h"
+# include "../minilibx_opengl_20191021/mlx.h"
 
-# define MAP_SIZE 50
-# define MAP_BUFFER 25
-# define X_EVENT_KEY_PRESS 2
-# define X_EVENT_EXIT 17
-# define KEY_W 13
-# define KEY_A 0
-# define KEY_S 1
-# define KEY_D 2
-# define KEY_ESC 53
+#define  Floor 0xFFFFFF
+#define  Ceiling 0x000000
 
-typedef struct s_location {
-	int	x;
-	int	y;
-}	t_location;
+#define  EPS            (1e-6)
+#define  is_zero(d)     (fabs(d) < EPS)
+#define  deg2rad(d)     ((d)*M_PI/180.0)    /* degree to radian */
+#define  rad2deg(d)     ((d)*180.0/M_PI)    /* radian to degree */
+#define  min(a,b)       ((a)<(b)? (a):(b))
+#define  max(a,b)       ((a)>(b)? (a):(b))
+#define  KEY_LEFT	123
+#define  KEY_RIGHT	124
+#define  KEY_W 13
+#define  KEY_A 0
+#define  KEY_S 1
+#define	 KEY_D 2
+#define	 X_EVENT_KEY_PRESS 2
+#define  IMG_SIZE 4
 
-typedef struct s_img {
-	void	*img_ptr;
-	int		width;
-	int		height;
-}	t_img;
+#define  SX         400     /* screen width */
+#define  SY         250     /* screen height */
+#define  FOV        60      /* field of view (in degree) */
+#define  FOV_H      deg2rad(FOV)
+#define  FOV_V      (FOV_H*(double)SY/(double)SX)
+#define  WALL_H     1.0
+#define  ROT_UNIT   0.03
+#define  MOVE_UNIT  0.1
+#define  _2PI       2 * M_PI /* 2 * M_PI */
 
-typedef struct s_map {
-	char	**map;
-	char	*err_msg;
-	int		start_count;
-	int		end_count;
-	int		item_count;
-	int		height;
-	int		width;
-}	t_map;
+enum { VERT, HORIZ };
 
-typedef struct s_win {
+typedef enum { DIR_N=0, DIR_E, DIR_W, DIR_S } dir_t;
+
+#define  MAPX   6
+#define  MAPY   5
+
+typedef struct	s_player
+{
+	double	x;
+	double	wx;
+	double	y;
+	double	wy;
+	double	th;
+}				t_player;
+
+typedef struct s_img
+{
+	void			*img;
+	unsigned int	*data;
+	int				bpp;
+	int				line_size;
+	int				endian;
+	int				w;
+	int				h;
+}			t_img;
+
+typedef struct s_info
+{
+	int			**map;
+	char		*path[IMG_SIZE];
+	int			floor_color;
+	int			ceilling_color;
+}			t_info;
+
+typedef struct	s_mlx
+{
 	void		*mlx_ptr;
-	void		*win_ptr;
-	t_map		map_info;
-	t_img		tile;
-	t_img		wall;
-	t_img		item;
-	t_img		door;
-	t_img		player;
-	t_img		bomb;
-	t_location	player_xy;
-	int			life;
-	int			move_count;
-	int			sprite_num;
-}	t_win;
+	void		*win;
+	void		*bg;
+	void		*bg_addr;
+	int			bg_bbp;
+	int			bg_line_l;
+	int			bg_endian;
+	t_player	player;
+	t_info		info;
+	t_img		background;
+	t_img		imgs[IMG_SIZE];
+}				t_mlx;
 
-char	*ft_strchr(const char *s, int c);
-void	ft_clear(t_win *mlx_vars);
-int		ft_strlen(char *str);
-void	ft_error(char *str);
-void	ft_reset_mlx_vars(t_win *mlx_vars);
-void	ft_check_map_first_line(char *temp, t_map *map_info);
-void	ft_check_middle_line_format(char *str, t_map *map_info);
-void	ft_check_map_last_line(char *line, t_map *map_info);
-void	ft_check_all_line(int fd, t_map *map_info);
-void	ft_check_map_format(char *filename, t_win *mlx_vars);
-char	*ft_itoa(int n);
-char	*ft_strjoin(char *str1, char *str2);
-int		ft_game_exit(t_win *mlx_vars);
-void	ft_fill_tile(t_win *mlx_vars);
-void	ft_print_map_image(t_win *mlx_vars, int x, int y);
-void	ft_print_map(t_win *mlx_vars);
-int		ft_render(t_win *mlx_vars);
-void	ft_reset_mlx_vars(t_win *mlx_vars);
-void	ft_move(t_win *mlx_vars, int keycode);
-int		ft_control_press_key(int keycode, t_win *mlx_vars);
-void	ft_init_game(t_win *mlx_vars);
-void	ft_malloc_map(char *filename, t_win *mlx_vars);
+
 
 #endif
